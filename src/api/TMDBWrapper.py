e5c0d2c2bf9a54365410edc45bae764abb4f6ed9
Dean Freeman <dean.freemanwilde@gmail.com>
2023-12-18 15:08:18 -0500

    src.api update
    started writing the class TMDBWrapper

---
+++ src/api/TMDBWrapper.py
@@ -0,0 +1,33 @@
+from tmdbv3api import TMDb, Movie, TV
+
+API_KEY = "a3a7bab86fcaab64c6a13deccc2c5134"
+
+class TMDBWrapper():
+    def __init__(self) -> None:
+        self.initialize()
+
+    def initialize(self):
+        tmdb = TMDb()
+        tmdb.api_key = API_KEY
+        tmdb.language = 'en'
+        self.movie()
+        self.series()
+    
+    @property
+    def movie(self):
+        self._movie = Movie()
+        return self._movie
+
+    @property
+    def series(self):
+        self._series = TV()
+        return self._series
+
+    def search(self, media_type, query):
+        try:
+            property_instance = getattr(self, media_type)
+        except AttributeError("Attribute {media_type} does not exist") as e:
+            raise e
+        results = property_instance.search(query)
+        # Process the search results as needed
+        return results
